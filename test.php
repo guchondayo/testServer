ae973c9117fa3bf3146aeb2da1eb2d466f2ec269
“guchondayo” <uto.to.be.engineer@gmail.com>
2023-01-22 13:19:23 +0900

    first commit

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+    echo "Hello, World!";
+
+    // PHPを使って簡易サーバーを立ち上げる方法
+    // 1.PHPfairuwo作成　echo
+    // 2.サーバを立ち上げる↓コマンド
+    // php -S 127.0.0.1:1192 test.php
+
+    // https://find-a.jp/seotimes/php-web-server/
+    // https://web-camp.io/magazine/archives/96988
+
+?>
